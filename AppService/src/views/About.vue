b76ac7b01d5b141cac252288143c3a0967667f53
nopains <nopains@126.com>
2019-11-09 08:23:12 +0800

    【添加依赖】添加vue-i18n插件

--- AppService/src/views/About.vue
+++ AppService/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>跳雀</h1>
+    <p>前端技术专家，专注前端开发10年，目前技术方向：前端微服务</p>
   </div>
 </template>
--- AppService/src/views/Home.vue
+++ AppService/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="技术分享"/>
   </div>
 </template>
 
