9d939a7a8ad3e65c5d123a75422d1a16d7f7c700
nopains <nopains@126.com>
2019-11-09 22:54:23 +0800

    【添加打包文件】

---
+++ AppService/dist/precache-manifest.d518348780e17595ba2291355506e2c8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "86977652c4e66b0654ce",
+    "url": "/js/about.8fa8b483.js"
+  },
+  {
+    "revision": "106f1fc057f835a6f02e",
+    "url": "/css/app.15ea206b.css"
+  },
+  {
+    "revision": "106f1fc057f835a6f02e",
+    "url": "/js/app.24bafdc2.js"
+  },
+  {
+    "revision": "b1c5dec2d842ec61283a",
+    "url": "/css/chunk-vendors.46f94e1b.css"
+  },
+  {
+    "revision": "b1c5dec2d842ec61283a",
+    "url": "/js/chunk-vendors.d3d6ecea.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "63cfef4872a3b17ca2c0ea38517ed6d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
